0f71b6ccb188fb8904199d138a23a5ee34088114
K Cartlidge <code@kcartlidge.com>
2018-08-17 22:03:20 +0100

    Add scanner

---
+++ Luthor.Tests/ScannerTests.cs
@@ -0,0 +1,96 @@
+using FluentAssertions;
+using Luthor;
+using System.Text;
+using Xunit;
+
+namespace Luther.Tests
+{
+    public class ScannerTests
+    {
+        private const string source = "<html>\n<body>\n <h1>Hello world.</h1>\n</body>\n</html>";
+
+        [Fact]
+        public void WithEmptySource_HasNoContent()
+        {
+            // Arrange.
+            var scanner = new Scanner(string.Empty);
+
+            // Act.
+            var hasContent = scanner.HasMore();
+
+            // Assert.
+            hasContent.Should().Be(false);
+        }
+
+        [Fact]
+        public void WithSource_HasSource()
+        {
+            // Arrange.
+            var scanner = new Scanner(source);
+            var result = new StringBuilder();
+
+            // Act.
+            while (scanner.HasMore())
+            {
+                result.Append(scanner.GetNext());
+            }
+
+            // Assert.
+            result.ToString().Should().Be(source);
+        }
+
+        [Fact]
+        public void GetNext_PassedEnd_ReturnsNull()
+        {
+            // Arrange.
+            var scanner = new Scanner(source);
+
+            // Act.
+            while (scanner.HasMore())
+            {
+                scanner.GetNext();
+            }
+            var result = scanner.GetNext();
+
+            // Assert.
+            result.HasValue.Should().Be(false);
+        }
+
+        [Fact]
+        public void GetNext_HasNoMore_EndOfSourceIsTrue()
+        {
+            // Arrange.
+            var scanner = new Scanner(source);
+            var result = new StringBuilder();
+
+            // Act.
+            while (scanner.HasMore())
+            {
+                result.Append(scanner.GetNext());
+            }
+
+            // Assert.
+            result.ToString().Length.Should().Be(source.Length);
+            scanner.HasMore().Should().Be(false);
+            scanner.EndOfSource().Should().Be(true);
+        }
+
+        [Fact]
+        public void GetCurrentPosition_GetsOffset()
+        {
+            // Arrange.
+            var source = "Line 1\nLine 2\nLine 3";
+            var scanner = new Scanner(source);
+
+            // Act.
+            for (var i = 1; i < source.Length; i++)
+            {
+                scanner.GetNext();
+            }
+            var location = scanner.GetCurrentPosition();
+
+            // Assert.
+            location.Should().Be(19);
+        }
+    }
+}
---
+++ Luthor/Scanner.cs
@@ -0,0 +1,41 @@
+namespace Luthor
+{
+    public class Scanner
+    {
+        private readonly string _source;
+        private readonly int _maxOffset = 0;
+        private int _offset = 0;
+
+        public Scanner(string source)
+        {
+            _source = source;
+            _maxOffset = _source.Length - 1;
+            _offset = 0;
+        }
+
+        public bool HasMore()
+        {
+            return _offset <= _maxOffset;
+        }
+
+        public bool EndOfSource()
+        {
+            return !HasMore();
+        }
+
+        public char? GetNext()
+        {
+            if (EndOfSource())
+            {
+                return null;
+            }
+
+            return _source[_offset++];
+        }
+
+        public int GetCurrentPosition()
+        {
+            return _offset;
+        }
+    }
+}
