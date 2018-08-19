e4de6c876cf3c7d88661fa906d668e2d9bde8d76
K Cartlidge <code@kcartlidge.com>
2018-08-19 22:27:22 +0100

    Add strings etc to Lexer

--- Luthor.Tests/LexerTests.cs
+++ Luthor.Tests/LexerTests.cs
@@ -78,5 +78,85 @@ namespace Luthor.Tests
                 TokenTypes.EOF
             });
         }
+
+        [Fact]
+        public void WithStrings_ReturnsStringRuns()
+        {
+            // Arrange.
+            Setup("A \"simple string\" and 'single quotes' and `back-ticks`.");
+
+            // Act.
+            var result = lexer.GetTokens();
+
+            // Assert.
+            result[2].Content.ToString().Should().Be("\"simple string\"");
+            result[6].Content.ToString().Should().Be("'single quotes'");
+            result[10].Content.ToString().Should().Be("`back-ticks`");
+            result[2].TokenType.Should().Be(TokenTypes.String);
+            result[6].TokenType.Should().Be(TokenTypes.String);
+            result[10].TokenType.Should().Be(TokenTypes.String);
+        }
+
+        [Fact]
+        public void WithUnterminatedString_ReturnsStringRun()
+        {
+            // Arrange.
+            Setup("An 'unterminated string");
+
+            // Act.
+            var result = lexer.GetTokens();
+
+            // Assert.
+            result[2].Content.ToString().Should().Be("'unterminated string");
+            result[2].TokenType.Should().Be(TokenTypes.String);
+        }
+
+        [Fact]
+        public void WithString_WithLineBreak_ReturnsStringRunIncludingLineBreak()
+        {
+            // Arrange.
+            Setup("A 'string with a \nnew line embedded'.");
+
+            // Act.
+            var result = lexer.GetTokens();
+
+            // Assert.
+            result[2].Content.ToString().Should().Be("'string with a \nnew line embedded'");
+            result[2].TokenType.Should().Be(TokenTypes.String);
+        }
+
+        [Fact]
+        public void WithString_WithEmbeddedStrings_ReturnsSingleStringRun()
+        {
+            // Arrange.
+            Setup("A \"simple string `with` 'embedded' strings\".");
+
+            // Act.
+            var result = lexer.GetTokens();
+
+            // Assert.
+            result[2].Content.ToString().Should().Be("\"simple string `with` 'embedded' strings\"");
+            result[2].TokenType.Should().Be(TokenTypes.String);
+        }
+
+        [Fact]
+        public void WithWhitespace_RequestCompressed_ReturnsCompressed()
+        {
+            // Arrange.
+            Setup("One two   three  \t \r four");
+
+            // Act.
+            var result = lexer.GetTokens(true);
+
+            // Assert.
+            result[1].Content.ToString().Should().Be(" ");
+            result[3].Content.ToString().Should().Be(" ");
+            result[5].Content.ToString().Should().Be(" ");
+            result[6].Content.ToString().Should().Be("four");
+            result[1].TokenType.Should().Be(TokenTypes.Whitespace);
+            result[3].TokenType.Should().Be(TokenTypes.Whitespace);
+            result[5].TokenType.Should().Be(TokenTypes.Whitespace);
+            result[6].TokenType.Should().Be(TokenTypes.Letters);
+        }
     }
 }
--- Luthor/Lexer.cs
+++ Luthor/Lexer.cs
@@ -1,5 +1,7 @@
 using Luthor.Models;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Luthor
 {
@@ -9,6 +11,7 @@ namespace Luthor
         private const string digits = "0123456789";
         private const string symbols = "!£$%^&*()-_=+[]{};:'@#~,.<>/?\\|";
         private const string whitespace = " \t";
+        private const string quotes = "'\"`";
 
         private readonly Scanner scanner;
         private int line;
@@ -22,7 +25,7 @@ namespace Luthor
             column = 1;
         }
 
-        public List<Token> GetTokens()
+        public List<Token> GetTokens(bool compressWhitespace = false)
         {
             var tokens = new List<Token>();
             var location = new Location
@@ -34,6 +37,7 @@ namespace Luthor
 
             while (scanner.HasMore())
             {
+                // Consume one char and set the location.
                 ch = scanner.GetNext().Value;
                 location = new Location
                 {
@@ -43,7 +47,14 @@ namespace Luthor
                 };
                 column += 1;
 
-                if (ch == (char)13)
+                if (quotes.IndexOf(ch) > -1)
+                {
+                    // Start string, read all until the next *matching* string token.
+                    var token = Token.Create(tokens, location, TokenTypes.String, ch);
+                    ConsumeUntil(token, ch);
+                    continue;
+                }
+                else if (ch == (char)13)
                 {
                     // Ignore carriage returns.
                     continue;
@@ -58,7 +69,14 @@ namespace Luthor
                 else
                 {
                     // Consume specific types.
-                    if (Consume(tokens, TokenTypes.Whitespace, location, whitespace)) continue;
+                    if (Consume(tokens, TokenTypes.Whitespace, location, whitespace))
+                    {
+                        if (compressWhitespace)
+                        {
+                            tokens.Last().Content = new StringBuilder(" ");
+                        }
+                        continue;
+                    }
                     if (Consume(tokens, TokenTypes.Letters, location, chars)) continue;
                     if (Consume(tokens, TokenTypes.Digits, location, digits)) continue;
                     if (Consume(tokens, TokenTypes.Symbols, location, symbols)) continue;
@@ -81,18 +99,36 @@ namespace Luthor
 
         private bool Consume(List<Token> tokens, TokenTypes tokenType, Location location, string charset)
         {
-            if (charset.IndexOf(ch) == -1) return false;
+            // If we're done (other than \r), exit and flag nothing happened.
+            if (charset.IndexOf(ch) == -1 && ch != (char)13) return false;
 
+            // Start consuming.
             var token = Token.Create(tokens, location, tokenType, ch);
             while (scanner.HasMore())
             {
+                // Repeat as long as we're not about to hit something else.
                 var next = scanner.PeekNext();
-                if (charset.IndexOf(next.Value) == -1) break;
+                if (charset.IndexOf(next.Value) == -1 && next.Value != (char)13) break;
 
                 token.Content.Append(scanner.GetNext().Value);
                 column += 1;
             }
             return true;
+        }
+
+        private void ConsumeUntil(Token token, char endChar)
+        {
+            while (scanner.HasMore())
+            {
+                // Repeat until we hit the end char.
+                var next = scanner.GetNext();
+                token.Content.Append(next.Value);
+                column += 1;
+                if (endChar == next.Value)
+                {
+                    break;
+                }
+            }
         }
     }
 }
